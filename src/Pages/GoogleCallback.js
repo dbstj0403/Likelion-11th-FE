16343999a1ec0e8a333a28607218a8246e9de1ad
Jiseok Son <jiseokson@gmail.com>
2023-07-11 15:54:02 +0900

    Fix: Google OAuth API URL 등

--- src/Pages/GoogleCallback.js
+++ src/Pages/GoogleCallback.js
@@ -1,48 +1,55 @@
-import axios from 'axios'
-import React from 'react';
-import { useState, useEffect } from 'react';
+import axios from "axios";
+import React from "react";
+import { useState, useEffect } from "react";
 
-export default function GoogleCallback () {
-  
-    const CODE = new URL(window.location.href).searchParams.get("code");
-    console.log('Code:', CODE);
+export default function GoogleCallback() {
+  const CODE = new URL(window.location.href).searchParams.get("code");
+  console.log("Code:", CODE);
 
-    const [userInfor, setUserInfor] = useState([]);
+  const [userInfor, setUserInfor] = useState([]);
 
-    const sendToken = async () => {
-       try {
-          const response = await axios.post('/accounts/github/token/', 
-          {code: CODE});
-          console.log(response);
-          localStorage.setItem('access_token', response.data.access);
-          localStorage.setItem('refresh_token', response.data.refresh);
-        }
-        catch(error){
-            console.log('Code sending error!');
-        }
-    }
-
-    const googleGetInfo = async () => {
-        try{
-            const response = await axios.get('/accounts', {headers: {Authorization: `Bearer ${localStorage.getItem('access_token')}`}});
-            console.log(response);
-            setUserInfor(response.data); 
-            console.log('User information:', userInfor);
-        }
-        catch(error)
+  const sendToken = async () => {
+    try {
+      const response = await axios.post(
+        "http://localhost:8000/auth/google/token",
         {
-            console.log('Login error!');
+          code: CODE,
         }
+      );
+      console.log(response);
+      localStorage.setItem("access_token", response.data.access);
+      localStorage.setItem("refresh_token", response.data.refresh);
+    } catch (error) {
+      console.log("Code sending error!");
     }
+  };
 
-    useEffect(() => {const googleLogin = async () => { 
-        await sendToken(); 
-        await googleGetInfo(); };
-        googleLogin();}, []);
-
-    return (
-          <div>
-            <h1>구글 로그인!</h1>
-          </div>
-        );
+  const googleGetInfo = async () => {
+    try {
+      const response = await axios.get("http://localhost:8000/accounts", {
+        headers: {
+          Authorization: `Bearer ${localStorage.getItem("access_token")}`,
+        },
+      });
+      console.log(response);
+      setUserInfor(response.data);
+      console.log("User information:", userInfor);
+    } catch (error) {
+      console.log("Login error!");
     }
+  };
+
+  useEffect(() => {
+    const googleLogin = async () => {
+      await sendToken();
+      await googleGetInfo();
+    };
+    googleLogin();
+  }, []);
+
+  return (
+    <div>
+      <h1>구글 로그인!</h1>
+    </div>
+  );
+}
